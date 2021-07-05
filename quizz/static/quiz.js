fb67bd5998466c954d926b80e3962ba90d27b6bb
Tiago21700980 <a21700980@alunos.ulht.pt>
2021-07-05 02:04:03 +0100

    Quizz 68% feito + files da treta lol

--- quizz/static/quiz.js
+++ quizz/static/quiz.js
@@ -6,6 +6,53 @@ const url = window.location.href
 const quizBox = document.getElementById('quiz-box')
 const scoreBox = document.getElementById('score-box')
 const resultBox = document.getElementById('result-box')
+const timerBox = document.getElementById('timer-box')
+
+const activateTimer = (time) => {
+    console.log(time)
+
+    if(time.toString().length < 2) {
+        timerBox.innerHTML = `<b>0${time}:00</b>`
+    }else{
+        timerBox.innerHTML = `<b>${time}:00</b>`
+    }
+    let minutes = time - 1
+    let seconds = 60
+    let displaySeconds
+    let displayMinutes
+
+    const timer = setInterval(()=>{
+        seconds --
+        if (seconds < 0) {
+            seconds = 59
+            minutes --
+        }
+        if (minutes.toString().length < 2) {
+            displayMinutes = '0' + minutes
+        } else {
+            displayMinutes = minutes
+        }
+
+        if(seconds.toString().length < 2){
+            displaySeconds = '0'+ seconds
+        }else{
+            displaySeconds = seconds
+        }
+        if(minutes === 0 && seconds === 0){
+            timerBox.innerHTML= "<b>00:00</b>"
+            setTimeout(()=>{
+                clearInterval(timer)
+                alert('Time is Over')
+                sendData()
+            },500)
+
+        }
+
+        timerBox.innerHTML = `<b>${displayMinutes}:${displaySeconds}</b>`
+        },1000
+    )
+
+}
 
 $.ajax({
     type: 'GET',
@@ -29,6 +76,7 @@ $.ajax({
             }
 
         })
+        activateTimer(response.time)
     },
     error: function (error){
         console.log(error)
