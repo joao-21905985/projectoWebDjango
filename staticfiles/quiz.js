3f102e57b87fdc3d4944add26cb874a1410f66e3
Tiago21700980 <a21700980@alunos.ulht.pt>
2021-07-25 00:08:32 +0100

    Quizz 68% feito + files da treta lol

--- staticfiles/quiz.js
+++ staticfiles/quiz.js
@@ -6,12 +6,59 @@ const url = window.location.href
 const quizBox = document.getElementById('quiz-box')
 const scoreBox = document.getElementById('score-box')
 const resultBox = document.getElementById('result-box')
+const timerBox = document.getElementById('timer-box')
+
+const activateTimer = (time) => {
+    console.log(time)
+
+    if(time.toString().length < 2) {
+        timerBox.innerHTML = `<b>0${time}:00</b>`
+    }else{
+        timerBox.innerHTML = `<b>${time}:00</b>`
+    }
+    let minutes = time - 1
+    let seconds = 60
+    let displaySeconds
+    let displayMinutes
+
+    const timer = setInterval(()=>{
+        seconds --
+        if (seconds < 0) {
+            seconds = 59
+            minutes --
+        }
+        if (minutes.toString().length < 2) {
+            displayMinutes = '0' + minutes
+        } else {
+            displayMinutes = minutes
+        }
+
+        if(seconds.toString().length < 2){
+            displaySeconds = '0'+ seconds
+        }else{
+            displaySeconds = seconds
+        }
+        if(minutes === 0 && seconds === 0){
+            timerBox.innerHTML= "<b>00:00</b>"
+            setTimeout(()=>{
+                clearInterval(timer)
+                alert('Time is Over')
+                sendData()
+            },500)
+
+        }
+
+        timerBox.innerHTML = `<b>${displayMinutes}:${displaySeconds}</b>`
+        },1000
+    )
+
+}
 
 $.ajax({
     type: 'GET',
     url: `${url}data`,
     success: function (response){
-        //console.log(response)
+        console.log(response)
         let data = response.data
         data.forEach(element =>{
             for (const [question, answers] of Object.entries(element)){
@@ -29,6 +76,7 @@ $.ajax({
             }
 
         })
+        activateTimer(response.time)
     },
     error: function (error){
         console.log(error)
@@ -63,7 +111,7 @@ const sendData = () =>{
             console.log(results)
             quizForm.classList.add('not-visible')
 
-            scoreBox.innerHTML = `${response.passed ? 'GZ' : 'gl next time :('} Your result is ${response.score.toFixed(2)} %`
+            scoreBox.innerHTML = `${response.passed ? 'GZ' : 'gl next time'} Your result is ${response.score.toFixed(2)} %`
 
             results.forEach(res=>{
                 const resDiv = document.createElement("div")
